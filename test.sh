c5295c5063626a0bb4d2695f4182e44bd44f2b43
Samson Hu <samson@contextlogic.com>
2018-04-10 23:35:22 -0700

    done

---
+++ test.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+env=$1
+file=""
+fails=""
+if [[ "${env}" == "stage" ]]; then
+	file="docker-compose-dev.yml"
+elif [[ "${env}" == "dev" ]]; then
+	file="docker-compose-dev.yml"
+elif [[ "${env}" == "prod" ]]; then
+	file="docker-compose-prod.yml"
+else
+	echo "USAGE: sh test.sh environment_name"
+	echo "* environment_name: must either be 'dev', 'stage', or 'prod'"
+	exit 1
+fi
+inspect() {
+if [ $1 -ne 0 ]; then
+220End-to-End Test Setup
+fails="${fails} $2"
+fi
+}
+docker-compose -f $file run users python manage.py test
+inspect $? users
+docker-compose -f $file run users flake8 project
+inspect $? users-lint
+if [[ "${env}" == "dev" ]]; then
+docker-compose -f $file run client npm test -- --coverage
+inspect $? client
+fi
+if [ -n "${fails}" ]; then
+echo "Tests failed: ${fails}"
+exit 1
+else
+echo "Tests passed!"
+exit 0
+fi
