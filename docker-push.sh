5fd45f01debbd857db1cba9e63b4a56862d1fcb9
Samson Hu <samson@contextlogic.com>
2018-04-11 14:03:25 -0700

    l

--- docker-push.sh
+++ docker-push.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]
-then
-	if [ "$TRAVIS_BRANCH" == "staging" ] || \
-		[ "$TRAVIS_BRANCH" == "production" ]
+	then
+		if [ "$TRAVIS_BRANCH" == "staging" ] || \
+			[ "$TRAVIS_BRANCH" == "production" ]
 		then
 			curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 			unzip awscli-bundle.zip
@@ -12,12 +12,12 @@ then
 			eval $(aws ecr get-login --region us-west-1 --no-include-email)
 			export TAG=$TRAVIS_BRANCH
 			export REPO=$AWS_ACCOUNT_ID.dkr.ecr.us-west-1.amazonaws.com
-	fi
-	if [ "$TRAVIS_BRANCH" == "staging" ] || \
-		[ "$TRAVIS_BRANCH" == "production" ]
-		then
+		fi
+		if [ "$TRAVIS_BRANCH" == "staging" ] || \
+			[ "$TRAVIS_BRANCH" == "production" ]
+			then
 			# users
-			docker build $USERS_REPO -t $USERS:$COMMIT -f Dockerfile-prod
+			docker build $USERS_REPO -t $USERS:$COMMIT -f Dockerfile-$DOCKER_ENV
 			docker tag $USERS:$COMMIT $REPO/$USERS:$TAG
 			docker push $REPO/$USERS:$TAG
 			# users db
@@ -25,12 +25,14 @@ then
 			docker tag $USERS_DB:$COMMIT $REPO/$USERS_DB:$TAG
 			docker push $REPO/$USERS_DB:$TAG
 			# client
-			docker build $CLIENT_REPO -t $CLIENT:$COMMIT -f Dockerfile-prod --build-arg REACT_APP_USERS_SERVICE_URL=TBD
+			docker build $CLIENT_REPO -t $CLIENT:$COMMIT -f Dockerfile-$DOCKER_ENV --build-
+			arg REACT_APP_USERS_SERVICE_URL=TBD
 			docker tag $CLIENT:$COMMIT $REPO/$CLIENT:$TAG
 			docker push $REPO/$CLIENT:$TAG
 			# swagger
-			docker build $SWAGGER_REPO -t $SWAGGER:$COMMIT -f Dockerfile-prod $SWAGGER_DIR
+			docker build $SWAGGER_REPO -t $SWAGGER:$COMMIT -f Dockerfile-$DOCKER_ENV $SWAGG
+			ER_DIR
 			docker tag $SWAGGER:$COMMIT $REPO/$SWAGGER:$TAG
 			docker push $REPO/$SWAGGER:$TAG
-	fi
+		fi
 fi
