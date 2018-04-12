0a1a5dcf12fffdce87124e2730793b6cb4c3d092
Samson Hu <samson@contextlogic.com>
2018-04-11 17:38:02 -0700

    elb

--- docker-push.sh
+++ docker-push.sh
@@ -1,38 +1,48 @@
 #!/bin/sh
 if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]
-	then
-		if [ "$TRAVIS_BRANCH" == "staging" ] || \
-			[ "$TRAVIS_BRANCH" == "production" ]
-		then
-			curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
-			unzip awscli-bundle.zip
-			./awscli-bundle/install -b ~/bin/aws
-			export PATH=~/bin:$PATH
-			# add AWS_ACCOUNT_ID, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY env vars
-			eval $(aws ecr get-login --region us-west-1 --no-include-email)
-			export TAG=$TRAVIS_BRANCH
-			export REPO=$AWS_ACCOUNT_ID.dkr.ecr.us-west-1.amazonaws.com
-		fi
-		if [ "$TRAVIS_BRANCH" == "staging" ] || \
-			[ "$TRAVIS_BRANCH" == "production" ]
-			then
-			# users
-			docker build $USERS_REPO -t $USERS:$COMMIT -f Dockerfile-$DOCKER_ENV
-			docker tag $USERS:$COMMIT $REPO/$USERS:$TAG
-			docker push $REPO/$USERS:$TAG
-			# users db
-			docker build $USERS_DB_REPO -t $USERS_DB:$COMMIT -f Dockerfile
-			docker tag $USERS_DB:$COMMIT $REPO/$USERS_DB:$TAG
-			docker push $REPO/$USERS_DB:$TAG
-			# client
-			docker build $CLIENT_REPO -t $CLIENT:$COMMIT -f Dockerfile-$DOCKER_ENV --build-
-			arg REACT_APP_USERS_SERVICE_URL=TBD
-			docker tag $CLIENT:$COMMIT $REPO/$CLIENT:$TAG
-			docker push $REPO/$CLIENT:$TAG
-			# swagger
-			docker build $SWAGGER_REPO -t $SWAGGER:$COMMIT -f Dockerfile-$DOCKER_ENV $SWAGG
-			ER_DIR
-			docker tag $SWAGGER:$COMMIT $REPO/$SWAGGER:$TAG
-			docker push $REPO/$SWAGGER:$TAG
-		fi
+then
+  if [ "$TRAVIS_BRANCH" == "staging" ] || \
+     [ "$TRAVIS_BRANCH" == "production" ]
+  then
+    curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+    unzip awscli-bundle.zip
+    ./awscli-bundle/install -b ~/bin/aws
+    export PATH=~/bin:$PATH
+    # add AWS_ACCOUNT_ID, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY env vars
+    eval $(aws ecr get-login --region us-west-1 --no-include-email)
+    export TAG=$TRAVIS_BRANCH
+    export REPO=$AWS_ACCOUNT_ID.dkr.ecr.us-west-1.amazonaws.com
+  fi
+  if [ "$TRAVIS_BRANCH" == "staging" ]
+  then
+    export REACT_APP_USERS_SERVICE_URL="testdriven-staging-alb-1826342578.us-west-1.elb.amazonaws.com"
+  fi
+  if [ "$TRAVIS_BRANCH" == "production" ]
+  then
+    export REACT_APP_USERS_SERVICE_URL="http://testdriven-production-alb-1950288253.us-west-1.elb.amazonaws.com"
+    export DATABASE_URL="$AWS_RDS_URI"
+    export SECRET_KEY="$PRODUCTION_SECRET_KEY"
+  fi
+  if [ "$TRAVIS_BRANCH" == "staging" ] || \
+     [ "$TRAVIS_BRANCH" == "production" ]
+  then
+    # users
+    docker build $USERS_REPO -t $USERS:$COMMIT -f Dockerfile-$DOCKER_ENV
+    docker tag $USERS:$COMMIT $REPO/$USERS:$TAG
+    docker push $REPO/$USERS:$TAG
+    # users db
+    docker build $USERS_DB_REPO -t $USERS_DB:$COMMIT -f Dockerfile
+    docker tag $USERS_DB:$COMMIT $REPO/$USERS_DB:$TAG
+    docker push $REPO/$USERS_DB:$TAG
+    # client
+    docker build $CLIENT_REPO -t $CLIENT:$COMMIT -f Dockerfile-$DOCKER_ENV --build-arg REACT_APP_USERS_SERVICE_URL=$REACT_APP_USERS_SERVICE_URL
+    docker tag $CLIENT:$COMMIT $REPO/$CLIENT:$TAG
+    docker push $REPO/$CLIENT:$TAG
+    # swagger
+    docker build $SWAGGER_REPO -t $SWAGGER:$COMMIT -f Dockerfile-$DOCKER_ENV $SWAGGER_DIR
+    docker tag $SWAGGER:$COMMIT $REPO/$SWAGGER:$TAG
+    docker push $REPO/$SWAGGER:$TAG
+  fi
 fi
+
+>>>>>>> Stashed changes
